ac083c9cbb31c9cce00a9bd85648291db34a91b0
Charlie Wartnaby <charlie.wartnaby@dana.com>
2024-05-28 17:25:37 +0100

    Smaller test set to try feature knockout on other PC

--- leap_feat_eng.py
+++ leap_feat_eng.py
@@ -20,12 +20,12 @@ if debug:
     max_epochs = 3
 else:
     # Use very large numbers for 'all'
-    max_train_rows = 1000000000
+    max_train_rows = 1000000
     max_test_rows  = 1000000000
-    max_batch_size = 5000  # 5000 with pcuk151, 30000 greta
+    max_batch_size = 2000  # 5000 with pcuk151, 30000 greta
     patience = 3 # was 5 but saving GPU quota
     train_proportion = 0.9
-    max_epochs = 5
+    max_epochs = 8
 
 multitrain_params = {}
 
