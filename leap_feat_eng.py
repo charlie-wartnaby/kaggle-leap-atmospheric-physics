02a87b6a20261ac5298415ad46b922fe7e14fb71
Charlie Wartnaby <charlie@wartnaby.org>
2024-06-17 04:32:54 +0100

    Got better validation R2 with scalings over 5M, then model fit over 100K, now comparing with model fit over 500K

--- leap_feat_eng.py
+++ leap_feat_eng.py
@@ -22,7 +22,7 @@ if debug:
     max_epochs = 1
 else:
     # Use very large numbers for 'all'
-    max_train_rows = 100000
+    max_train_rows = 500000
     max_test_rows  = 1000000000
     max_batch_size = 20000  # 5000 with pcuk151, 30000 greta
     patience = 3 # was 5 but saving GPU quota
