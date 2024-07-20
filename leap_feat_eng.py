6328d5ae55562b0f9d89f8ae234016931b061910
Charlie Wartnaby <charlie@wartnaby.org>
2024-07-20 09:57:01 +0100

    Model structure experiment for writeup: with and without 1x1 layer, encoder/decoder

--- leap_feat_eng.py
+++ leap_feat_eng.py
@@ -48,15 +48,15 @@ import warnings
 
 # Settings
 debug                       = False
-do_test                     = True
-is_rerun                    = True
+do_test                     = False
+is_rerun                    = False
 do_analysis                 = True
 do_train                    = True
 do_feature_knockout         = False
 clear_batch_cache_at_start  = debug
 scale_using_range_limits    = False
 do_save_outputs_as_features = False
-do_use_outputs_as_features  = not do_save_outputs_as_features
+do_use_outputs_as_features  = False #not do_save_outputs_as_features
 do_merge_outputs_early      = False
 do_merge_outputs_late       = True
 use_encoder_decoder         = True
@@ -79,19 +79,20 @@ if debug:
     max_epochs                    = 1
 else:
     # Use very large numbers for 'all'
-    max_train_rows                = excess_number_of_rows
+    max_train_rows                = 1000000 # excess_number_of_rows
     max_test_rows                 = excess_number_of_rows
     max_output_feature_train_rows = excess_number_of_rows
     catboost_batch_size           = 20000
-    cnn_batch_size                = 2500  # To fit GPU with current model
+    cnn_batch_size                = 5000  # To fit GPU with current model
     patience                      = 5
-    train_proportion              = 0.999
-    max_epochs                    = 14
+    train_proportion              = 0.8
+    max_epochs                    = 30
 
 
 # For model parameters to form permutations of in hyperparameter search
 # Each entry is 'param_name' : [list of values for that parameter]
-multitrain_params = {} 
+multitrain_params = {"init_1x1"        : [False, True],
+                     "encoder_decoder" : [False, True]} 
 if debug and model_type == "catboost":
     multitrain_params = {'iterations' : [2]} # Otherwise too slow
 
